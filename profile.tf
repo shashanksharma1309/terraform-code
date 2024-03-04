ce3539620d98e725f0fd24fb972159cd21383e3c
shashanksharma1309 <159928443+shashanksharma1309@users.noreply.github.com>
2024-03-04 06:55:44 +0530

    profile.tf
    provider file

---
+++ profile.tf
@@ -0,0 +1,7 @@
+#it provide provider,region,access & secret key
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAVRUVVJO3NJHAWBS6"
+  secret_key = "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"
+}
+
