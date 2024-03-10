2145493801e9daf0c7cf841e2201e9bd4c27c69b
shashanksharma1309 <shashank953217@gmail.com>
2024-03-10 11:26:44 +0530

    upload

--- profile.tf
+++
@@ -1,7 +0,0 @@
-#It provide provider,region,access & secret key
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAVRUVVJO3NJHAWBS6"
-  secret_key = "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"
-}
-
